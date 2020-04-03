190a278153cbe817699dbd9535da29426c9ecf17
Carlos <charliemike31245@gmail.com>
2020-04-03 13:40:02 -0500

    home and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NosotrosComponent } from './Components/Nosotros/nosotros.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { ServiciosComponent } from './Components/servicios/servicios.component';
-import { ContactoComponent } from './Components/contacto/contacto.component'; 
+import { FooterComponent } from './Components/Footer/footer.component';
+import { ServiciosComponent } from './Components/Servicios/servicios.component';
+import { ContactoComponent } from './Components/Contacto/contacto.component'; 
 import { CardModule } from 'primeng/card';
 
 @NgModule({
